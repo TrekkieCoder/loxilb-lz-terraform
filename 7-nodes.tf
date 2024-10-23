75da674d0f92f9569a382d860d86c2cfeede19a8
Trekkie Coder <trekkie@netlox.io>
2024-10-23 15:00:07 +0000

    local-zone support

--- 7-nodes.tf
+++ 7-nodes.tf
@@ -130,6 +130,54 @@ resource "aws_eks_node_group" "lz-lb-nodes" {
   ]
 }
 
+resource "aws_eks_node_group" "lz-worker-nodes" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "worker-lz"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = [
+    #aws_subnet.public-us-east-1-atl-2a.id,
+    #aws_subnet.private-us-east-1-atl-2a.id
+    #aws_subnet.private-us-east-1a.id,
+    #aws_subnet.private-us-east-1b.id
+    aws_subnet.public-us-east-1a.id,
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["c6i.large"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 10
+    min_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 2
+  }
+
+  labels = {
+    node = "wlznode02"
+  }
+
+  # taint {
+  #   key    = "team"
+  #   value  = "devops"
+  #   effect = "NO_SCHEDULE"
+  # }
+
+  # launch_template {
+  #   name    = aws_launch_template.eks-with-disks.name
+  #   version = aws_launch_template.eks-with-disks.latest_version
+  # }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
 # launch template if required
 
 # resource "aws_launch_template" "eks-with-disks" {
